242ef05afa4e59843bebc194cb4d1222b7a0653b
Benjamin Kim <166325010+BenK03@users.noreply.github.com>
2026-01-02 01:03:33 -0500

    completed evaluate guess function.
    Gives feedback on which colour each letter should be coloured.

--- src/wordle/main.py
+++ src/wordle/main.py
@@ -1,6 +1,7 @@
 import argparse
 import random
 
+# puts all words into an array
 def load_words():
     words = []
     with open("data/words.txt", "r", encoding="utf-8") as file:
@@ -10,6 +11,28 @@ def load_words():
     
     return words
 
+# finds feedback for each letter
+def evaluate_guess(guess, target_word):
+    feedback = ["gray"] * 5
+    target_word_array = list(target_word)
+
+    # check greens
+    for i in range(5):
+        if guess[i] == target_word_array[i]:
+            feedback[i] = "green"
+            # covers duplicates
+            target_word_array[i] = None
+        
+    # check yellows
+    for i in range(5):
+        if feedback[i] == "gray" and guess[i] in target_word_array:
+            feedback[i] = "yellow"
+            # covers duplicates
+            target_word_array[target_word_array.index(guess[i])] = None
+
+    return feedback
+
+# game logic/start game
 def start_game():
     words = load_words()
     target_word = random.choice(words)
@@ -23,20 +46,26 @@ def start_game():
             print("Invalid guess. Only 5 letter words.")
             continue
             
-        if not guess.isalpha():
+        elif not guess.isalpha():
             print("Invalid guess. Only letters.")
             continue
 
-        if guess == target_word:
-            #temp (add play again after)
+        elif guess == target_word:
+            #temp (add play again after and green colouring)
             print("Congrats, you got it!")
             return
-
+        
+        else:
+            # guess is valid but not target
+            # add green colouring
+            evaluate_guess(guess, target_word)
+            
         guesses_left -= 1
 
     # temp (add play again after)
     print("Game over")
 
+# controls commands
 def main():
     parser = argparse.ArgumentParser(description="Wordle game")
     
@@ -49,7 +78,6 @@ def main():
     # test
     if args.start:
         start_game()
-
 
 
 if __name__ == "__main__":
